a3e6b4ab4ad4119c1769fa5506839c6293c054d4
Anbu34 <monarchfxlabs@gmail.com>
2025-09-10 17:21:47 +0000

    finished generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "my grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", crushed, "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = ["before class", "when i was sleeping", "while i was exercising", "during my lunch"];
+
+  let randomNumber1 = (Math.random() * who.length);
+  let randomNumber2 = (Math.random() * action.length);
+  let randomNumber3 = (Math.random() * what.length);
+  let randomNumber4 = (Math.random() * when.length);
+  
+  let theWho = who[randomNumber1];
+  let theAction = action[randomNumber2]
+  let theWhat = what[randomNumber3]
+  let theWhen = when[randomNumber4]
+
+  let excuse = theWho + theAction + theWhat + theWhen ;
+
+  console.log(excuse);
+  let HTMLElement = document.querySelector("#excuse");
+
+  HTMLElement.innerHTML = excuse ;
+
 };
