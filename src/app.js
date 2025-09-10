673bc63bab7a734357d5aaaf888b82cb7588c871
Anbu34 <monarchfxlabs@gmail.com>
2025-09-10 21:39:38 +0000

    ec finished generator2

--- src/app.js
+++ src/app.js
@@ -5,30 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
 
   let who = ["The dog", "my grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", crushed, "broke"];
+  let action = ["ate", "peed on", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
   let when = ["before class", "when i was sleeping", "while i was exercising", "during my lunch"];
 
-  let randomNumber1 = (Math.random() * who.length);
-  let randomNumber2 = (Math.random() * action.length);
-  let randomNumber3 = (Math.random() * what.length);
-  let randomNumber4 = (Math.random() * when.length);
+function generateExcuse(){
+
+  let whorandom = who[Math.floor(Math.random() * who.length)];
+  let actionrandom = action[Math.floor(Math.random() * action.length)];
+  let whatrandom = what[Math.floor(Math.random() * what.length)];
+  let whenrandom = when[Math.floor(Math.random() * when.length)];
   
-  let theWho = who[randomNumber1];
-  let theAction = action[randomNumber2]
-  let theWhat = what[randomNumber3]
-  let theWhen = when[randomNumber4]
 
-  let excuse = theWho + theAction + theWhat + theWhen ;
+  let excuse = `My excuse: ${whorandom} ${actionrandom} ${whatrandom} ${whenrandom} `;
+  document.getElementById("excuse").innerHTML = excuse;
 
-  console.log(excuse);
-  let HTMLElement = document.querySelector("#excuse");
+}
 
-  HTMLElement.innerHTML = excuse ;
+window.onload = generateExcuse;
 
-};
